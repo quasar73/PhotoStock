0b9e6157470e477650deefb38705be333e8f074c
Vladislav Brezgun <nextydev@gmail.com>
2020-08-26 12:50:10 +0300

    [phst-1] - created photo model and categories enum

---
+++ PhotoStock.DataBase/Models/Photo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoStock.DataBase.Models
+{
+	enum Categories
+	{
+		Nature,
+		Animals,
+		Human,
+		Weapon,
+		Thing,
+		Other
+	}
+	class Photo
+	{
+		public string Name { get; set; }
+		public string Path { get; set; }
+		public string UserId { get; set; }
+		public DateTime UploadDate { get; set; }
+		public Categories Category { get; set; }
+	}
+}

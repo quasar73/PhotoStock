d230da4fb112670ded58e7e83e4b3b1302d0d514
Vladislav Brezgun <nextydev@gmail.com>
2020-08-27 08:39:52 +0300

    [phst-2] - added roles initilizer

---
+++ PhotoStock.Web/RolesInitilizer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace PhotoStock.Web
+{
+	public class RolesInitilizer
+	{
+		public static async Task InitilizeAsync(RoleManager<IdentityRole> roleManager)
+		{
+			if (await roleManager.FindByNameAsync("admin") == null)
+				await roleManager.CreateAsync(new IdentityRole("admin"));
+			if (await roleManager.FindByNameAsync("user") == null)
+				await roleManager.CreateAsync(new IdentityRole("user"));
+		}
+	}
+}

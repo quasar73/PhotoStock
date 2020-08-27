59e150b245543f8216b71e833a545c308f0cd107
Vladislav Brezgun <nextydev@gmail.com>
2020-08-27 11:49:35 +0300

    [phst-3] - added IRepository interface and photo repository class

---
+++ PhotoStock.DataBase/Repositories/PhotoRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PhotoStock.DataBase.Models;
+
+namespace PhotoStock.DataBase.Repositories
+{
+	class PhotoRepository : IRepository<Photo>
+	{
+		private readonly ApplicationContext context;
+		public PhotoRepository(ApplicationContext context)
+		{
+			this.context = context;
+		}
+		public async Task CreateAsync(Photo entity)
+		{
+			await context.Photos.AddAsync(entity);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task DeleteAsync(Photo entity)
+		{
+			context.Photos.Remove(entity);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task<Photo> GetByIdAsync(int id)
+		{
+			return await context.Photos.FirstOrDefaultAsync<Photo>(p => p.Id == id);
+		}
+
+		public async Task<IEnumerable> GetListAsync()
+		{
+			return await context.Photos.ToListAsync<Photo>();
+		}
+
+		public async Task UpdateAsync(Photo entity)
+		{
+			var target = context.Photos.FirstOrDefault(p => p.Id == entity.Id);
+			if(entity != null)
+			{
+				target.Name = entity.Name;
+				target.Path = entity.Path;
+				target.UploadDate = entity.UploadDate;
+				target.Category = entity.Category;
+				context.Photos.Update(target);
+				await context.SaveChangesAsync();
+			}
+		}
+	}
+}
